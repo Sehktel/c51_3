151d783aece940e48a15a851cbc77e5fb18861fd
Sehktel <24711055+Sehktel@users.noreply.github.com>
2025-04-24 19:18:54 +0300

    Обновление проекта c51cc: исправлена опечатка в описании компилятора, добавлены зависимости для логирования, изменены пути ресурсов для тестирования, увеличен объем памяти JVM до 2ГБ. Также добавлены комментарии на русском языке для улучшения понимания кода.

--- c51cc/project.clj
+++ c51cc/project.clj
@@ -1,28 +1,34 @@
 (defproject c51cc "0.1.0-SNAPSHOT"
-  :description "C51 Clojure Compiller"
+  :description "C51 Clojure Compiler"
   :url "http://github.com/Sehktel/c51cc"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
+  
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [instaparse "1.4.10"]]
+                 [instaparse "1.4.10"]
+                 [org.clojure/tools.logging "1.2.1"]
+                 [log4j/log4j "1.2.17"]]
   
   ;; Настройки для тестирования
   :test-paths ["test"]
   :source-paths ["src"]
+  :resource-paths ["c51code"]
   
   ;; Профили для разработки
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.3.0"]
                                   [org.clojure/test.check "1.1.1"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns user}}
-             :test {:resource-paths ["test-resources"]}}
+             
+             :test {:resource-paths ["c51code"]
+                    :env {:file-c "c51code/simple_example.c"
+                          :include-path "c51code"}}}
   
   ;; Плагины
   :plugins [[lein-cloverage "1.2.2"]
             [lein-kibit "0.1.8"]
             [lein-bikeshed "0.5.2"]
-            [com.github.clj-kondo/lein-clj-kondo "0.2.5"]
-            ]
+            [com.github.clj-kondo/lein-clj-kondo "0.2.5"]]
   
   :bikeshed {
     :max-line-length 125
@@ -32,7 +38,7 @@
   }
   
   ;; Настройки JVM
-  :jvm-opts ["-Xmx1g"]
+  :jvm-opts ["-Xmx2g"]
   
   ;; Точка входа для запуска
   :main c51cc.core
@@ -44,3 +50,9 @@
                       (:manual m)
                       (= (str (:name m)) "test-manual-parse")))
   })
+  ;; ;; Селекторы тестов
+  ;; :test-selectors {:default (complement :integration)
+  ;;                  :integration :integration
+  ;;                  :ast :ast
+  ;;                  :all (constantly true)}
+  ;; )
