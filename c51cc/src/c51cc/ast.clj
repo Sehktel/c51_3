8ee63b4b1608d6c48b9e0226359b568b2ebe58ff
Sehktel <24711055+Sehktel@users.noreply.github.com>
2025-04-24 19:16:59 +0300

    ast_2

--- c51cc/src/c51cc/ast.clj
+++ c51cc/src/c51cc/ast.clj
@@ -11,7 +11,16 @@
             [c51cc.logger :as log]
             [clojure.java.io :as io]
             [clojure.string :as str]
-            [clojure.pprint :as pprint]))
+            [clojure.pprint :as pprint]
+            [clojure.stacktrace :as stacktrace]))
+
+(defn- get-test-file
+  "Получает путь к тестовому файлу из переменной окружения"
+  []
+  (or (System/getenv "TEST_FILE")
+      (System/getProperty "test.file")
+      (throw (ex-info "Не указан тестовый файл" 
+                      {:message "Установите переменную окружения TEST_FILE"}))))
 
 (defn- trace-include-file-search
   "Трассировка поиска включаемых файлов с расширенной диагностикой"
@@ -22,7 +31,8 @@
   (let [search-paths [(io/file base-path filename)
                       (io/file (System/getProperty "user.dir") filename)
                       (io/file (System/getProperty "user.dir") "include" filename)
-                      (io/file (System/getProperty "user.dir") "src" "include" filename)]]
+                      (io/file (System/getProperty "user.dir") "src" "include" filename)
+                      (io/file (System/getProperty "user.dir") "c51code" filename)]]
     
     (log/info "Пути поиска:")
     (doseq [path search-paths]
@@ -73,43 +83,58 @@
 
 (defn process-c-file
   "Полный цикл обработки файла с расширенной диагностикой"
-  [source-path & {:keys [include-path] 
-                  :or {include-path (System/getProperty "user.dir")}}]
-  (log/info "Начало полного цикла обработки файла")
+  [& {:keys [source-path include-path] 
+      :or {source-path (get-test-file)
+           include-path (System/getProperty "user.dir")}}]
+  (log/info "=== Начало полного цикла обработки файла ===")
   (log/set-debug-level! :DEBUG)
   
-  (log/info "Полный путь к исходному файлу:" (.getAbsolutePath (io/file source-path)))
-  (log/debug "Путь включения:" include-path)
-  
-  (let [source-content (read-file-content source-path)
-        
-        _ (log/info "Размер исходного файла:" (count source-content) "байт")
-        _ (log/trace "Содержимое исходного файла:\n" source-content)
-        
-        preprocessed-code (with-redefs [preprocessor/read-include-file custom-read-include-file]
-                             (preprocessor/preprocess source-content 
-                                                     :base-path include-path))]
+  (try
+    (log/info "Stage 1: Подготовка и идентификация файлов")
+    (log/info "Полный путь к исходному файлу:" (.getAbsolutePath (io/file source-path)))
+    (log/debug "Путь включения:" include-path)
     
-    (log/info "Результат препроцессинга:")
-    (log/debug "Размер после препроцессинга:" (count preprocessed-code) "байт")
-    (log/trace "Содержимое после препроцессинга:\n" preprocessed-code)
-    
-    (let [tokens (lexer/tokenize preprocessed-code)]
-      (log/info "Токенизация завершена")
-      (log/debug "Количество токенов:" (count tokens))
-      (log/trace "Токены:\n" tokens)
+    (log/info "Stage 2: Чтение и подготовка содержимого файлов")
+    (let [source-content (read-file-content source-path)]
+      (log/info "Размер исходного файла:" (count source-content) "байт")
+      (log/trace "Содержимое исходного файла:\n" source-content)
       
-      (let [ast (parser/parse tokens)]
-        (log/info "Построение AST завершено")
-        (log/debug "Сложность AST:" (count ast))
-        (log/trace "Абстрактное синтаксическое дерево:\n" ast)
+      (log/info "Stage 3: Препроцессинг")
+      (let [preprocessed-code (with-redefs [preprocessor/read-include-file custom-read-include-file]
+                               (preprocessor/preprocess source-content 
+                                                       :base-path include-path))]
+        (log/info "Результат препроцессинга:")
+        (log/debug "Размер после препроцессинга:" (count preprocessed-code) "байт")
+        (log/trace "Содержимое после препроцессинга:\n" preprocessed-code)
         
-        {:source-path source-path
-         :include-path include-path
-         :source-content source-content
-         :preprocessed-code preprocessed-code
-         :tokens tokens
-         :ast ast}))))
+        (log/info "Stage 4: Токенизация")
+        (let [tokens (lexer/tokenize preprocessed-code)]
+          (log/info "Токенизация завершена")
+          (log/debug "Количество токенов:" (count tokens))
+          (log/trace "Токены:\n" tokens)
+          
+          (log/info "Stage 5: Построение AST")
+          (let [ast (parser/parse tokens)]
+            (log/info "Построение AST завершено")
+            (log/debug "Сложность AST:" (count ast))
+            (log/trace "Абстрактное синтаксическое дерево:\n" ast)
+            
+            (log/info "=== Обработка файла успешно завершена ===")
+            {:source-path source-path
+             :include-path include-path
+             :source-content source-content
+             :preprocessed-code preprocessed-code
+             :tokens tokens
+             :ast ast}))))
+    (catch Exception e
+      (log/error "=== Критическая ошибка при обработке файла ===")
+      (log/error "Сообщение:" (.getMessage e))
+      (log/error "Стек вызовов:" (with-out-str (stacktrace/print-stack-trace e)))
+      (throw (ex-info "Ошибка при обработке файла" 
+                     {:source-path source-path
+                      :include-path include-path
+                      :error (.getMessage e)
+                      :exception e})))))
 
 (defn generate-ast
   "Генерация абстрактного синтаксического дерева с расширенной обработкой"
@@ -141,18 +166,15 @@
 (defn -main
   "Точка входа для демонстрации возможностей AST"
   [& args]
-  (log/set-level! :DEBUG)
+  (log/log-level! :DEBUG)
   (log/info "Инициализация модуля AST")
   
-  (when (seq args)
-    (let [file-path (first args)
-          base-path (or (second args) ".")]
-      (try
-        (let [result (process-c-file file-path :include-path base-path)]
-          (println "Сгенерированный AST:")
-          (println (pretty-print-ast (:ast result)))
-          (println "\nАнализ AST:")
-          (println (analyze-ast (:ast result))))
-        (catch Exception e
-          (log/error "Ошибка при генерации AST: " (.getMessage e))
-          (System/exit 1))))))
+  (try
+    (let [result (process-c-file)]
+      (println "Сгенерированный AST:")
+      (println (pretty-print-ast (:ast result)))
+      (println "\nАнализ AST:")
+      (println (analyze-ast (:ast result))))
+    (catch Exception e
+      (log/error "Ошибка при генерации AST: " (.getMessage e))
+      (System/exit 1))))
--- c51cc/test/c51cc/ast_test.clj
+++ c51cc/test/c51cc/ast_test.clj
@@ -1,11 +1,110 @@
-(ns c51cc.ast-test
+(ns c51cc.ast_test
   "Модуль тестирования генерации и анализа абстрактного синтаксического дерева (AST)"
   (:require [clojure.test :refer :all]
             [c51cc.ast :as ast]
             [c51cc.logger :as log]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.stacktrace :as stacktrace]))
 
-;; Вспомогательные функции для тестирования
+;; Константы для конфигурации тестирования
+(def ^:private default-test-file-env "TEST_FILE")
+(def ^:private default-include-path-env "INCLUDE_PATH")
+
+(defn- print-env-vars
+  "Печатает значения переменных окружения для отладки"
+  []
+  (let [env-vars [default-test-file-env 
+                  default-include-path-env 
+                  "TEST_FILE" 
+                  "FILE_C" 
+                  "INCLUDE_PATH" 
+                  ]]
+    (log/info "=== Переменные окружения ===")
+    (doseq [var-name env-vars]
+      (log/debug (format "%-20s: %s" 
+                       var-name 
+                       (or (System/getenv var-name) 
+                           (System/getProperty var-name) 
+                           "НЕ УСТАНОВЛЕНА"))))
+    (log/info "=== Системные свойства ===")
+    (doseq [[prop-name prop-value] (sort (System/getProperties))]
+      (when (re-find #"(user|file|path|dir)" (str prop-name))
+        (log/debug (format "%-40s: %s" prop-name prop-value))))
+    (log/info "==========================")))
+
+(defn- get-env-var
+  "Получает значение переменной окружения с возможностью указания значения по умолчанию"
+  ([var-name] (get-env-var var-name nil))
+  ([var-name default-value]
+   (or (System/getenv var-name)
+       (System/getProperty (str "env." var-name))
+       default-value)))
+
+(defn- log-environment-variables 
+  "Логирование переменных окружения для отладки"
+  []
+  (let [env-vars ["FILE_C" "INCLUDE_PATH" "TEST_FILE"]]
+    (log/info "=== Переменные окружения ===")
+    (doseq [var-name env-vars]
+      (log/info (str "Переменная окружения " var-name ": " 
+                     (or (System/getenv var-name) 
+                         (System/getProperty var-name) 
+                         "НЕ УСТАНОВЛЕНА"))))
+    
+    (log/info "=== Системные свойства ===")
+    (doseq [[prop-name prop-value] 
+            (->> (System/getProperties)
+                 (into {})
+                 (filter (fn [[k _]] (re-find #"(user|file|path|dir)" (str k))))
+                 (sort-by first))]
+      (log/info (format "%-40s: %s" prop-name prop-value)))
+    
+    (log/info "==========================")))
+
+(defn- get-test-file
+  "Получает путь к тестовому файлу из переменных окружения
+    
+   Приоритет источников:
+   1. Переменная окружения TEST_FILE
+   2. Переменная окружения FILE_C
+   3. Системное свойство test.file
+   4. Значение по умолчанию"
+  []
+  (log/info "Stage 1: Определение тестового файла")
+  (let [env-vars ["TEST_FILE" "FILE_C" "test.file"]
+        test-file-candidates (keep #(get-env-var % nil) env-vars)
+        default-file "c51code/simple_example.c"
+        
+        ;; Выбираем первый существующий файл или используем значение по умолчанию
+        test-file (or 
+                   (first (filter #(.exists (io/file %)) test-file-candidates))
+                   default-file)]
+    
+    (log/info "Кандидаты тестового файла:" (pr-str test-file-candidates))
+    (log/info "Выбран тестовый файл:" test-file)
+    
+    ;; Проверка существования файла с более подробной диагностикой
+    (let [file (io/file test-file)]
+      (when-not (.exists file)
+        (log/error "Тестовый файл не найден:" test-file)
+        (log/error "Текущий рабочий каталог:" (System/getProperty "user.dir"))
+        (log/error "Абсолютный путь файла:" (.getAbsolutePath file))
+        (throw (ex-info "Тестовый файл не найден" 
+                       {:file test-file
+                        :current-dir (System/getProperty "user.dir")
+                        :absolute-path (.getAbsolutePath file)
+                        :candidates test-file-candidates}))))
+    test-file))
+
+(defn- get-include-path
+  "Получает путь включения из переменных окружения"
+  []
+  (log/info "Stage 2: Определение пути включения")
+  (let [include-path (get-env-var default-include-path-env "c51code")]
+    (log/info "Используется путь включения:" include-path)
+    include-path))
+
 (defn- create-test-file 
   "Создает временный тестовый файл с заданным содержимым"
   [content]
@@ -24,118 +123,83 @@
 (def ^:private simple-c-code 
   "Простой тестовый код на языке C
    для проверки базовой функциональности AST"
-  "#include <stdio.h>
+  "#include <reg51.h>
 
-int main() {
-    int x = 10;
-    printf(\"Hello, World! %d\", x);
-    return 0;
-}")
+  void main(void) { ; }
+")
 
 (def ^:private complex-c-code
   "Более сложный тестовый код с различными конструкциями"
-  "#include <stdlib.h>
-#define MAX_SIZE 100
-
-void process_array(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] > MAX_SIZE) {
-            arr[i] = MAX_SIZE;
-        }
-    }
-}
-
-int main() {
-    int numbers[MAX_SIZE];
-    int count = 0;
-    
-    while (count < MAX_SIZE) {
-        numbers[count] = rand() % 200;
-        count++;
-    }
-    
-    process_array(numbers, MAX_SIZE);
-    return 0;
-}")
+  "#include <reg51.h>
+  #define A 5
+  #define B 10
+  void main(void) {
+    char C;
+    C = A + B;
+  }")
 
 (deftest test-generate-ast
   "Тест генерации абстрактного синтаксического дерева"
-  (testing "Генерация AST для простого кода"
-    (let [test-file (create-test-file simple-c-code)
-          result (ast/process-c-file (.getAbsolutePath test-file))]
-      (is (map? result) "Результат должен быть картой")
-      (is (contains? result :ast) "Результат должен содержать AST")
-      (is (vector? (:ast result)) "AST должен быть вектором")
-      (is (pos? (count (:ast result))) "AST не должен быть пустым")
-      (cleanup-test-file test-file)))
-  
-  (testing "Генерация AST для сложного кода"
-    (let [test-file (create-test-file complex-c-code)
-          result (ast/process-c-file (.getAbsolutePath test-file))]
-      (is (map? result) "Результат должен быть картой")
-      (is (contains? result :ast) "Результат должен содержать AST")
-      (is (vector? (:ast result)) "AST должен быть вектором")
-      (is (pos? (count (:ast result))) "AST не должен быть пустым")
-      (cleanup-test-file test-file))))
-
-(deftest test-ast-analysis
-  "Тест анализа абстрактного синтаксического дерева"
-  (testing "Анализ AST простого кода"
-    (let [test-file (create-test-file simple-c-code)
-          result (ast/process-c-file (.getAbsolutePath test-file))
-          analysis (ast/analyze-ast (:ast result))]
-      (is (map? analysis) "Результат анализа должен быть картой")
-      (is (contains? analysis :node-types) "Анализ должен содержать типы узлов")
-      (is (contains? analysis :total-nodes) "Анализ должен содержать общее количество узлов")
-      (is (contains? analysis :max-depth) "Анализ должен содержать максимальную глубину")
-      (is (pos? (:total-nodes analysis)) "Должны быть узлы в AST")
-      (cleanup-test-file test-file)))
-  
-  (testing "Анализ AST сложного кода"
-    (let [test-file (create-test-file complex-c-code)
-          result (ast/process-c-file (.getAbsolutePath test-file))
-          analysis (ast/analyze-ast (:ast result))]
-      (is (map? analysis) "Результат анализа должен быть картой")
-      (is (contains? analysis :node-types) "Анализ должен содержать типы узлов")
-      (is (contains? analysis :total-nodes) "Анализ должен содержать общее количество узлов")
-      (is (contains? analysis :max-depth) "Анализ должен содержать максимальную глубину")
-      (is (pos? (:total-nodes analysis)) "Должны быть узлы в AST")
-      (cleanup-test-file test-file))))
+  (testing "Генерация AST из переменной окружения"
+    (log/info "=== Начало теста генерации AST ===")
+    (log-environment-variables)
+    (try
+      (let [test-file (get-test-file)
+            include-path (get-include-path)
+            _ (log/info "Stage 3: Генерация AST")
+            _ (log/info "Тестовый файл:" test-file)
+            _ (log/info "Путь включения:" include-path)
+            result (ast/process-c-file 
+                    :source-path test-file 
+                    :include-path include-path)]
+        
+        (log/info "Результат обработки файла:")
+        (log/info "Путь к исходному файлу:" (:source-path result))
+        (log/info "Путь включения:" (:include-path result))
+        (log/info "Размер исходного содержимого:" (count (:source-content result)) "байт")
+        (log/info "Размер препроцессированного кода:" (count (:preprocessed-code result)) "байт")
+        (log/info "Количество токенов:" (count (:tokens result)))
+        (log/info "Количество узлов AST:" (count (:ast result)))
+        
+        (is (map? result) "Результат должен быть картой")
+        (is (contains? result :ast) "Результат должен содержать AST")
+        (is (pos? (count (:ast result))) "AST не должен быть пустым")
+        (log/info "=== Тест успешно завершен ==="))
+      (catch Exception e
+        (log/error "Критическая ошибка в тесте:" (.getMessage e))
+        (log/error "Стек вызовов:" (with-out-str (stacktrace/print-stack-trace e)))
+        (throw e)))))
 
 (deftest test-pretty-print-ast
   "Тест визуализации абстрактного синтаксического дерева"
-  (testing "Визуализация AST простого кода"
-    (let [test-file (create-test-file simple-c-code)
-          result (ast/process-c-file (.getAbsolutePath test-file))
+  (testing "Визуализация AST из переменной окружения"
+    (let [test-file (get-test-file)
+          include-path (get-include-path)
+          result (ast/process-c-file 
+                  :source-path test-file 
+                  :include-path include-path)
           pretty-print (ast/pretty-print-ast (:ast result))]
+      
+      ;; Логирование результатов pretty-print
+      (log/info "=== Результат Pretty Print AST ===")
+      (log/info "Длина строки:" (count pretty-print))
+      (log/info "Первые 500 символов:\n" (subs pretty-print 0 (min 500 (count pretty-print))))
+      
+      ;; Проверки
       (is (string? pretty-print) "Визуализация должна быть строкой")
       (is (pos? (count pretty-print)) "Визуализация не должна быть пустой")
-      (cleanup-test-file test-file)))
-  
-  (testing "Визуализация AST сложного кода"
-    (let [test-file (create-test-file complex-c-code)
-          result (ast/process-c-file (.getAbsolutePath test-file))
-          pretty-print (ast/pretty-print-ast (:ast result))]
-      (is (string? pretty-print) "Визуализация должна быть строкой")
-      (is (pos? (count pretty-print)) "Визуализация не должна быть пустой")
-      (cleanup-test-file test-file))))
-
-(deftest test-error-handling
-  "Тест обработки ошибок при генерации AST"
-  (testing "Обработка несуществующего файла"
-    (is (thrown? Exception 
-                 (ast/process-c-file "/path/to/nonexistent/file.c"))
-        "Должно быть выброшено исключение для несуществующего файла"))
-  
-  (testing "Обработка пустого файла"
-    (let [empty-file (create-test-file "")]
-      (is (thrown? Exception 
-                   (ast/process-c-file (.getAbsolutePath empty-file)))
-          "Должно быть выброшено исключение для пустого файла")
-      (cleanup-test-file empty-file))))
+      
+      ;; Дополнительные проверки структуры
+      (let [parsed-print (read-string pretty-print)]
+        (is (vector? parsed-print) "Pretty print должен быть читаемым вектором")
+        (is (< (count parsed-print) 11) "Должно быть не более 10 элементов в теле AST")))))
 
 (defn -main
   "Точка входа для запуска тестов AST"
   [& args]
-  (log/set-level! :DEBUG)
-  (run-tests 'c51cc.ast-test)) 
+  (log/log-level! :DEBUG)
+  (log/debug "Начало тестирования AST")
+  ;; Печать переменных окружения перед запуском тестов
+  (print-env-vars)
+  
+  (run-tests 'c51cc.ast_test))
